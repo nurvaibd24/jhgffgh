e689d315c77e706f8a8618db89b35ac4a8402a70
nurvaibd24 <84995015+nurvaibd24@users.noreply.github.com>
2021-06-30 04:06:41 +0600

    Update crowdnet1.sh

--- crowdnet1.sh
+++ crowdnet1.sh
@@ -19,7 +19,7 @@ password=`head -n2 $1 | tail -1`
 
 HOST='64.20.61.229'
 USER='vpnnetcl_crowdnet'
-PASS='2K7.{VTQq-z@"'
+PASS='2K7.{VTQq-z@'
 DB='vpnnetcl_crowdnet'
 
 Query="SELECT user_name FROM users WHERE user_name='$username' AND user_encryptedPass=md5('$password') AND is_freeze='0' AND user_duration > 0"
